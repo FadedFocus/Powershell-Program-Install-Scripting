0f9900a21adf27cc70b279b8b14ed426ddedc018
Henry <105020132+FadedFocus@users.noreply.github.com>
2025-12-07 12:33:48 -0600

    Update download_installers.ps1
    ver. 4.0.4 changes:
    slimeVRInstalled changed back to localAppData

--- download_installers.ps1
+++ download_installers.ps1
@@ -43,7 +43,7 @@ function Test-WiresharkInstalled {
 }
 
 function Test-slimeVRInstalled {
-    $exe = "$env:ProgramFiles\slimeVR/slimeVR.exe"
+    $exe = "$env:LocalAppData\slimeVR/slimeVR.exe"
     return (Test-Path $exe)
 }
 
